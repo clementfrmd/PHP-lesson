ed4a8274cfc88695ac7a9389dba03b2c12ba8ae7
Clément <fermaudclement@gmail.com>
2019-02-23 14:04:35 +0100

    commit

---
+++ boucles.php
@@ -0,0 +1,22 @@
+<?php
+
+$repetitions =0; // initialisation
+
+while ($repetitions < 10)
+{
+    echo '<p> Je ne dois pas copier sur on voisin. ' . $repetitions . ' fois</p>';
+    $repetitions++;
+}
+
+?>
+
+<?php
+
+// $repetitions =0;
+
+for ($repetitions = 0 ; $repetitions < 20 ; $repetitions++) // initialisation
+{
+    echo '<p> Je ne dois pas copier sur on voisin. ' . $repetitions . ' fois</p>';
+}
+
+?>
---
+++ variable-2.php
@@ -0,0 +1,11 @@
+<?php
+$nombre = 2 + 4;
+$nombre = 5 - 1;
+$nombre = 3 * 5;
+$nombre = 10 / 2;
+$nombre = 3 * 5 + 1;
+$nombre = (1 + 2) * 2;
+$resultat = ($nombre + 5) * $nombre;
+
+echo ''. $resultat .'';
+?>
