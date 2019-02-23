27bae87ae94bc65e46d5ee722bf10c1cb3aaaf99
Clément <fermaudclement@gmail.com>
2019-02-23 16:44:58 +0100

    commit
    Exemple d'archi de site en php

---
+++ page2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Plus d'infos sur moi</title>
+    </head>
+ 
+    <body>
+ 
+    <?php include("head.php"); ?>
+    
+    <?php include("menu.php"); ?>
+
+     <!-- Le corps -->
+    
+     <div id="corps">
+        <h1>Plus d'infos sur moi</h1>
+        
+        <p>
+            Sur cette page, je vais vous parler un peu plus dans le détail de Moi.<br />
+            Qui suis-je Quelle est ma vie, mon oeuvre ?
+        </p>
+    </div>
+    
+    
+    <?php include("footer.php"); ?>
+    
+    </body>
+</html>
